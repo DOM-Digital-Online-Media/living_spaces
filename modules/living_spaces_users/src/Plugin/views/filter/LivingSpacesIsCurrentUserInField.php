5e735a0baf8f7cdb905d816a8b766aba04197bd9
Bohdan Minich <b.minich@dev-branch.com>
2022-11-08 11:56:48 +0200

    Ref #40009 Added field_moderators to solr index event_invites_and_chat_appointments. Fixed custom filter for filtering current user in fields to the event_invites_and_chat_appointments view. Added configurations to the istall for new views.

---
+++ modules/living_spaces_users/src/Plugin/views/filter/LivingSpacesIsCurrentUserInField.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Drupal\living_spaces_users\Plugin\views\filter;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\filter\FilterPluginBase;
+
+/**
+ * Filter fields list for the current user.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("living_spaces_is_current_user_in_field")
+ */
+class LivingSpacesIsCurrentUserInField extends FilterPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+
+    $options['field_is_current_user'] = ['default' => NULL];
+
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    $form['field_is_current_user'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Field machine names'),
+      '#description' => $this->t('Fields to check is contains current user. Each one should me on the new line.'),
+      '#default_value' => $this->options['field_is_current_user'],
+      '#required' => TRUE,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::validateOptionsForm($form, $form_state);
+
+    $option = 'field_is_current_user';
+    if (empty(trim($form_state->getValues()['options'][$option]))) {
+      $form_state->setError($form[$option], $this->t('Provided value cannot be empty.'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $this->ensureMyTable();
+
+    /** @var \Drupal\views_contextual_filters_or\Plugin\views\query\ExtendedSearchApiQuery $query */
+    $query = $this->query;
+
+    $current_user_id = \Drupal::currentUser()->id();
+    $condition = ($query->createConditionGroup('OR'));
+    foreach (explode("\n", $this->options['field_is_current_user']) as $field) {
+      $field = trim($field);
+      if (!empty($field)) {
+        $condition->addCondition($field, $current_user_id, '=');
+      }
+    }
+    $query->addWhere($this->options['group'], $condition);
+
+  }
+
+}
