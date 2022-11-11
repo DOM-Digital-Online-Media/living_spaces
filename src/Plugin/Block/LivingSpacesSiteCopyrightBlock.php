2de27733313b959497e72e93b495cadc7361ca9c
Bohdan Minich <b.minich@dev-branch.com>
2022-11-11 19:05:49 +0200

    Ref #40962 Removed odd footer regions, added 'Footer left' and 'Footer right' instead. Added block LivingSpacesSiteCopyrightBlock to show configurable site name, get default value for it from the site name configuration and place block to the 'Footer left' region. Added block LivingSpacesFooterLogoBlock to show image as site logo and placed this block to the 'Footer right' region. Added Footer menu and place it in the 'Footer left' region.

---
+++ src/Plugin/Block/LivingSpacesSiteCopyrightBlock.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Drupal\living_spaces\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a site copyright block.
+ *
+ * @Block(
+ *   id = "living_spaces_site_copyright_block",
+ *   admin_label = @Translation("Site Copyright"),
+ *   category = @Translation("Living Spaces")
+ * )
+ */
+class LivingSpacesSiteCopyrightBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The file storage service.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $fileStorage;
+
+  /**
+   * Constructs a LivingSpacesPageTitleBlock block.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   Request stack that controls the lifecycle of requests.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $configFactory) {
+    $this->configFactory = $configFactory;
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    $sitename = $this->configFactory->get('system.site')->get('name');
+
+    return [
+      'sitename' => empty($sitename) ? 'Living Spaces' : $sitename,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['sitename'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Site name'),
+      '#default_value' => $this->configuration['sitename'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['sitename'] = $form_state->getValue('sitename');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build['content'] = [
+      '#markup' => "&#169; " . $this->configuration['sitename'],
+    ];
+    return $build;
+  }
+
+}
