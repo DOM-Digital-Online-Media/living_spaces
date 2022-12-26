869dff62f3b4d4882a24be85d18088167edc409c
Victor Golovachek <v.golovachek@dev-branch.com>
2022-12-26 13:40:52 +0200

    refs #40014 - Add service methods

--- modules/living_spaces_intranet/src/LivingSpacesBansManager.php
+++ modules/living_spaces_intranet/src/LivingSpacesBansManager.php
@@ -3,6 +3,8 @@
 namespace Drupal\living_spaces_intranet;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Component\Datetime\TimeInterface;
 
 /**
  * Manager for ban related methods.
@@ -17,13 +19,69 @@ class LivingSpacesBansManager implements LivingSpacesBansManagerInterface {
   protected $entityTypeManager;
 
   /**
+   * Returns the current_user service.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Returns the time service.
+   *
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  protected $time;
+
+  /**
    * LivingSpacesBansManager constructor.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   Provides an interface for entity type managers.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   Defines an account interface which represents the current user.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   Defines an interface for obtaining system time.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, AccountInterface $current_user, TimeInterface $time) {
     $this->entityTypeManager = $entity_type_manager;
+    $this->currentUser = $current_user;
+    $this->time = $time;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getUserBans(AccountInterface $user): array {
+    $storage = $this->entityTypeManager->getStorage('living_spaces_ban');
+    $query = $storage->getQuery();
+    $query->condition('target_user', $user->id());
+
+    if ($results = $query->execute()) {
+      return $storage->loadMultiple($results);
+    }
+
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUserBan(AccountInterface $user, array $data) {
+    $bundle = !empty($data['bundle']) ? $data['bundle'] : 'global';
+
+    if ($this->currentUser->hasPermission('administer ban') ||
+      $this->currentUser->hasPermission('create ' . $bundle . ' ban')
+    ) {
+      $storage = $this->entityTypeManager->getStorage('living_spaces_ban');
+
+      $ban = $storage->create([
+        'type' => $bundle,
+        'target_user' => $user->id(),
+        'title' => !empty($data['reason']) ? $data['reason'] : "Ban {$user->id()}",
+        'expire' => !empty($data['expire']) ? $data['expire'] : $this->time->getRequestTime(),
+      ]);
+      $ban->save();
+    }
   }
 
 }
