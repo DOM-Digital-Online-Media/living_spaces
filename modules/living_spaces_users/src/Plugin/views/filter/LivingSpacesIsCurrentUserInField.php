28a357cc3742894f3e8acd17eaab66c44dd8522e
Bohdan Minich <b.minich@dev-branch.com>
2023-02-28 13:09:04 +0200

    Ref #40009 Fixed the calling static method of Drupal, added dependency injection for the current user instead.

--- modules/living_spaces_users/src/Plugin/views/filter/LivingSpacesIsCurrentUserInField.php
+++ modules/living_spaces_users/src/Plugin/views/filter/LivingSpacesIsCurrentUserInField.php
@@ -4,6 +4,7 @@ namespace Drupal\living_spaces_users\Plugin\views\filter;
 
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\views\Plugin\views\filter\FilterPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Filter fields list for the current user.
@@ -13,6 +14,21 @@ use Drupal\views\Plugin\views\filter\FilterPluginBase;
  * @ViewsFilter("living_spaces_is_current_user_in_field")
  */
 class LivingSpacesIsCurrentUserInField extends FilterPluginBase {
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->currentUser = $container->get('current_user');
+    return $instance;
+  }
 
   /**
    * {@inheritdoc}
@@ -59,7 +75,7 @@ class LivingSpacesIsCurrentUserInField extends FilterPluginBase {
     /** @var \Drupal\views_contextual_filters_or\Plugin\views\query\ExtendedSearchApiQuery $query */
     $query = $this->query;
 
-    $current_user_id = \Drupal::currentUser()->id();
+    $current_user_id = $this->currentUser->id();
     $condition = ($query->createConditionGroup('OR'));
     foreach (explode(PHP_EOL, $this->options['field_is_current_user']) as $field) {
       $field = trim($field);
