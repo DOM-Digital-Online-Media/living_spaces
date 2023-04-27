697b066f3bd87bc230473972bcb9c43f6da28e77
Victor Golovachek <v.golovachek@dev-branch.com>
2023-04-27 15:36:03 +0300

    refs #45186 - Hid group types

--- modules/living_spaces_group/src/Form/LivingSpaceExcludeSpacesForm.php
+++ modules/living_spaces_group/src/Form/LivingSpaceExcludeSpacesForm.php
@@ -2,12 +2,11 @@
 
 namespace Drupal\living_spaces_group\Form;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\user\PermissionHandler;
-use Drupal\Core\Extension\ModuleHandlerInterface;
 
 /**
  * Form handler for exclude spaces settings.
@@ -15,34 +14,24 @@ use Drupal\Core\Extension\ModuleHandlerInterface;
 class LivingSpaceExcludeSpacesForm extends ConfigFormBase {
 
   /**
-   * Returns the user.permissions service.
+   * Returns the entity_type.manager service.
    *
-   * @var \Drupal\user\PermissionHandler
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
-  protected $userPermissions;
-
-  /**
-   * Returns the module_handler service.
-   *
-   * @var \Drupal\Core\Extension\ModuleHandlerInterface
-   */
-  protected $moduleHandler;
+  protected $entityTypeManager;
 
   /**
    * Constructs a new LivingSpaceExcludeSpacesForm object.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The factory for configuration objects.
-   * @param \Drupal\user\PermissionHandler $user_permissions
-   *   Provides the available permissions based on yml files.
-   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
-   *   Interface for classes that manage a set of enabled modules.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Provides an interface for entity type managers.
    */
-  public function __construct(ConfigFactoryInterface $config_factory, PermissionHandler $user_permissions, ModuleHandlerInterface $module_handler) {
+  public function __construct(ConfigFactoryInterface $config_factory, EntityTypeManagerInterface $entity_type_manager) {
     parent::__construct($config_factory);
 
-    $this->userPermissions = $user_permissions;
-    $this->moduleHandler = $module_handler;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
@@ -51,8 +40,7 @@ class LivingSpaceExcludeSpacesForm extends ConfigFormBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('config.factory'),
-      $container->get('user.permissions'),
-      $container->get('module_handler')
+      $container->get('entity_type.manager')
     );
   }
 
@@ -67,19 +55,25 @@ class LivingSpaceExcludeSpacesForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
-    return ['living_spaces_exclude_spaces'];
+    return ['living_spaces_group.exclude_spaces'];
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->configFactory->getEditable('living_spaces_exclude_spaces');
+    $config = $this->configFactory->getEditable('living_spaces_group.exclude_spaces');
+
+    $options = [];
+    /** @var \Drupal\group\Entity\GroupTypeInterface $type */
+    foreach ($this->entityTypeManager->getStorage('group_type')->loadMultiple() as $type) {
+      $options[$type->id()] = $type->label();
+    }
 
     $form['spaces'] = [
       '#type' => 'checkboxes',
       '#title' => $this->t('Spaces'),
-      '#options' => [],
+      '#options' => $options,
       '#default_value' => !empty($config->get('spaces')) ? $config->get('spaces') : [],
     ];
 
@@ -90,8 +84,16 @@ class LivingSpaceExcludeSpacesForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->configFactory->getEditable('living_spaces_exclude_spaces')
-      ->set('spaces', $form_state->getValue('spaces'))
+    if ($spaces = $form_state->getValue('spaces')) {
+      foreach ($spaces as $key => $value) {
+        if (empty($spaces[$key])) {
+          unset($spaces[$key]);
+        }
+      }
+    }
+
+    $this->configFactory->getEditable('living_spaces_group.exclude_spaces')
+      ->set('spaces', $spaces)
       ->save();
 
     parent::submitForm($form, $form_state);
