acc98ca1f4a922d825caec0055570bf99b463e84
Victor Golovachek <v.golovachek@dev-branch.com>
2023-07-11 16:11:44 +0300

    refs #39908 - Add ajax callback

--- modules/living_spaces_activity/js/living_spaces_activity_load.js
+++ modules/living_spaces_activity/js/living_spaces_activity_load.js
@@ -10,8 +10,24 @@
    */
   Drupal.behaviors.livingSpacesActivityLoad = {
     attach: function (context, settings) {
-      // @todo: call to the 'living_spaces_activity.get_persistent_messages' route for getting PDM messages for the current user (e.g. each 1 min).
-      console.log('we in');
+      setInterval(function() {
+        $.ajax({
+          url: Drupal.url('living-spaces-activity/get-persistent-messages/' + settings.user.uid),
+          type: 'POST',
+          contentType: 'application/json; charset=utf-8',
+          dataType: 'json',
+          success: function success(value) {
+            if (value.success) {
+              if ($('div[id^=block-views-block-message-persistent]').length) {
+                $('div[id^=block-views-block-message-persistent]').replaceWith(value.message);
+              }
+              else {
+                $('div.highlighted').append(value.message);
+              }
+            }
+          }
+        });
+      }, 60 * 1000);
     }
   };
 
