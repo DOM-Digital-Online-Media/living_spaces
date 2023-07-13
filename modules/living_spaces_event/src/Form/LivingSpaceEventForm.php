34d977a52eda86341195a6c9d189be7e81c2a87a
Victor Golovachek <v.golovachek@dev-branch.com>
2023-07-13 12:01:46 +0300

    refs #46178 - Add validation

--- modules/living_spaces_event/src/Form/LivingSpaceEventForm.php
+++ modules/living_spaces_event/src/Form/LivingSpaceEventForm.php
@@ -4,11 +4,33 @@ namespace Drupal\living_spaces_event\Form;
 
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Datetime\DrupalDateTime;
 
 /**
  * Form controller for the space event add and edit forms.
  */
 class LivingSpaceEventForm extends ContentEntityForm {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+
+    $start = $form_state->getValue('field_start_date');
+    $end = $form_state->getValue('field_end_date');
+
+    if (isset($start[0]['value'], $end[0]['value']) &&
+      $start[0]['value'] instanceof DrupalDateTime &&
+      $end[0]['value'] instanceof DrupalDateTime &&
+      $end[0]['value']->getTimestamp() < $start[0]['value']->getTimestamp()
+    ) {
+      $form_state->setErrorByName('field_start_date', $this->t('The end date must be later than the start date.'));
+      $form_state->setErrorByName('field_end_date');
+    }
+
+    return $this->entity;
+  }
 
   /**
    * {@inheritdoc}
