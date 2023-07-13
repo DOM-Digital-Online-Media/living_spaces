7960e2bb8a6431dac0c17ca84d77c5ace8912df5
Victor Golovachek <v.golovachek@dev-branch.com>
2023-07-13 16:53:58 +0300

    refs #46120 - Fix title

--- modules/living_spaces_group/src/LivingSpacesGroupEntityController.php
+++ modules/living_spaces_group/src/LivingSpacesGroupEntityController.php
@@ -15,10 +15,17 @@ class LivingSpacesGroupEntityController extends EntityController {
    * {@inheritdoc}
    */
   public function deleteTitle(RouteMatchInterface $route_match, EntityInterface $_entity = NULL) {
-    // @todo: load group content from route.
-    if ($entity = $this->doGetEntity($route_match, $_entity)) {
-      return $this->t('Delete %label', ['%label' => $entity->label()]);
+    /** @var \Symfony\Component\HttpFoundation\ParameterBag $parameters */
+    $parameters = $route_match->getRawParameters();
+
+    /** \Drupal\group\Entity\GroupContentInterface $content */
+    if ($parameters->get('group_content', NULL) &&
+      $content = $this->entityTypeManager->getStorage('group_content')->load($parameters->get('group_content'))
+    ) {
+      return $this->t('Delete %label', ['%label' => $content->getEntity()->label()]);
     }
+
+    return parent::deleteTitle($route_match, $_entity);
   }
 
 }
