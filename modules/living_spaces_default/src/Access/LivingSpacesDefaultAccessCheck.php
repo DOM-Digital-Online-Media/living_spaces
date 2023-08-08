a49abc70b21685033c899334985b59f72291877a
Victor Golovachek <v.golovachek@dev-branch.com>
2023-08-08 14:28:09 +0300

    refs #43466 - Fix access

--- modules/living_spaces_default/src/Access/LivingSpacesDefaultAccessCheck.php
+++ modules/living_spaces_default/src/Access/LivingSpacesDefaultAccessCheck.php
@@ -2,21 +2,22 @@
 
 namespace Drupal\living_spaces_default\Access;
 
-use Drupal\Core\Routing\Access\AccessInterface;
+use Drupal\Core\Routing\Access\AccessInterface as CoreAccessInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Access\AccessResult;
+use Symfony\Component\Routing\Route;
 
 /**
  * LivingSpacesDefaultAccessCheck class.
  */
-class LivingSpacesDefaultAccessCheck implements AccessInterface {
+class LivingSpacesDefaultAccessCheck implements CoreAccessInterface {
 
   /**
    * Check access for default content.
    */
-  public function access(RouteMatchInterface $route, AccountInterface $account) {
-    $group = $route->getParameter('group');
+  public function access(Route $route, RouteMatchInterface $route_match, AccountInterface $account) {
+    $group = $route_match->getParameter('group');
 
     if ($group && $group->get('is_default')->getString()) {
       return $account->hasPermission('manage living spaces default content') ? AccessResult::allowed() : AccessResult::forbidden();
