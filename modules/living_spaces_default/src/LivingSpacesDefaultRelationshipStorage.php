0b78d991932d0785436f82388fcfc2c2e2507d63
Victor Golovachek <v.golovachek@dev-branch.com>
2023-10-12 13:07:52 +0300

    refs #49207 - Fix validation

--- modules/living_spaces_default/src/LivingSpacesDefaultRelationshipStorage.php
+++ modules/living_spaces_default/src/LivingSpacesDefaultRelationshipStorage.php
@@ -14,7 +14,7 @@ class LivingSpacesDefaultRelationshipStorage extends GroupRelationshipStorage {
    * {@inheritdoc}
    */
   public function save(EntityInterface $entity) {
-    if ($entity->getGroup()->get('is_default')->getString()) {
+    if ($entity->getGroup()->get('is_default')->getString() && 'group_membership' == $entity->getPluginId()) {
       // We cannot add an entity to a default group.
       return FALSE;
     }
