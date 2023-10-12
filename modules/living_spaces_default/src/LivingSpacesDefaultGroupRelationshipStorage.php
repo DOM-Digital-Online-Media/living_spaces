0e965e71af0420f7500529a8436c7652167d2453
Victor Golovachek <v.golovachek@dev-branch.com>
2023-10-12 11:41:00 +0300

    refs #49207 - Alter storage

--- modules/living_spaces_default/src/LivingSpacesDefaultGroupRelationshipStorage.php
+++ modules/living_spaces_default/src/LivingSpacesDefaultGroupRelationshipStorage.php
@@ -5,7 +5,6 @@ namespace Drupal\living_spaces_default;
 use Drupal\Core\Entity\EntityStorageException;
 use Drupal\group\Entity\Storage\GroupRelationshipStorage;
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\group\Entity\GroupInterface;;
 
 /**
  * Defines the storage handler class for relationship entities.
@@ -15,12 +14,12 @@ class LivingSpacesDefaultGroupRelationshipStorage extends GroupRelationshipStora
   /**
    * {@inheritdoc}
    */
-  public function createForEntityInGroup(EntityInterface $entity, GroupInterface $group, $plugin_id, $values = []) {
-    if ($group->get('is_default')->getString()) {
+  public function save(EntityInterface $entity) {
+    if ($entity->getGroup()->get('is_default')->getString()) {
       throw new EntityStorageException('Cannot add an entity to a default group.');
     }
 
-    return parent::createForEntityInGroup($entity, $group, $plugin_id, $values);
+    return parent::save($entity);
   }
 
 }
